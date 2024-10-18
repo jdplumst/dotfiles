50af835ed0915f837b94f2c60617e975008a0edf
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-10-18 08:45:23 -0400

    Change neovim theme to catppuccin mocha

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -3,8 +3,8 @@ return { -- You can easily change to a different colorscheme.
   -- change the command in the config to whatever the name of that colorscheme is
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-  'folke/tokyonight.nvim',
-  -- 'catppuccin/nvim',
+  -- 'folke/tokyonight.nvim',
+  'catppuccin/nvim',
   -- 'EdenEast/nightfox.nvim',
   -- 'scottmckendry/cyberdream.nvim',
   priority = 1000, -- make sure to load this before all the other start plugins
@@ -14,15 +14,15 @@ return { -- You can easily change to a different colorscheme.
     -- vim.cmd.colorscheme 'terafox'
     -- vim.cmd.colorscheme 'carbonfox'
     -- vim.cmd.colorscheme 'cyberdream'
-    -- vim.cmd.colorscheme 'catppuccin-mocha'
-    local time = os.date '*t'
-    if time.hour >= 11 and time.hour < 20 then
-      -- vim.cmd.colorscheme 'catppuccin-macchiato'
-      vim.cmd.colorscheme 'tokyonight-moon'
-    else
-      -- vim.cmd.colorscheme 'catppuccin-mocha'
-      vim.cmd.colorscheme 'tokyonight-night'
-    end
+    vim.cmd.colorscheme 'catppuccin-mocha'
+    -- local time = os.date '*t'
+    -- if time.hour >= 11 and time.hour < 20 then
+    --   vim.cmd.colorscheme 'catppuccin-macchiato'
+    --   -- vim.cmd.colorscheme 'tokyonight-moon'
+    -- else
+    --   vim.cmd.colorscheme 'catppuccin-mocha'
+    --   -- vim.cmd.colorscheme 'tokyonight-night'
+    -- end
     -- You can configure highlights by doing something like
     vim.cmd.hi 'Comment gui=none'
   end,
