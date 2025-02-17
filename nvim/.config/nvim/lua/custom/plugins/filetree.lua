37c2f9c44748212c754d249c9332dbb176b9a7b0
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-02-17 14:22:38 -0500

    Make neotree smaller and place on the right

--- nvim/.config/nvim/lua/custom/plugins/filetree.lua
+++ nvim/.config/nvim/lua/custom/plugins/filetree.lua
@@ -2,13 +2,24 @@
 
 return {
   'nvim-neo-tree/neo-tree.nvim',
+  opts = {
+    window = {
+      position = 'right',
+      width = 30,
+    },
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  config = function()
-    require('neo-tree').setup {}
-  end,
+  -- config = function()
+  --   require('neo-tree').setup {}
+  -- end,
 }
