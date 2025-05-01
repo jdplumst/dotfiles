7c04d1bc675bd201d92c7509acedc49151d7694e
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-04-30 21:52:17 -0400

    Add oil to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -106,6 +106,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Oil
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
 -- Tabstop
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
