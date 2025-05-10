0aa76ba27173bfb0959cd7284a45ed5652225d4e
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-05-10 18:03:26 -0400

    Change neovim and ghostty colorschemes to catppuccin-mocha

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-  'folke/tokyonight.nvim',
-  -- 'catppuccin/nvim',
+  -- 'folke/tokyonight.nvim',
+  'catppuccin/nvim',
   -- 'EdenEast/nightfox.nvim',
   -- 'scottmckendry/cyberdream.nvim',
   -- 'rose-pine/neovim',
@@ -16,12 +16,12 @@ return {
   init = function()
     -- Load the colorscheme here.
     -- vim.cmd.colorscheme 'tokyonight-moon'
-    vim.cmd.colorscheme 'tokyonight-night'
+    -- vim.cmd.colorscheme 'tokyonight-night'
     -- vim.cmd.colorscheme 'nightfox'
     -- vim.cmd.colorscheme 'terafox'
     -- vim.cmd.colorscheme 'carbonfox'
     -- vim.cmd.colorscheme 'cyberdream'
-    -- vim.cmd.colorscheme 'catppuccin-mocha'
+    vim.cmd.colorscheme 'catppuccin-mocha'
     -- vim.cmd.colorscheme 'rose-pine'
     -- local time = os.date '*t'
     -- if time.hour >= 11 and time.hour < 20 then
